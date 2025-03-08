45bea5b9d5ccb9a59549bb5c934569f00ad46d23
Pradyumn Seth <pradyumnsethmzp123@gmail.com>
2025-03-08 07:54:00 +0530

    23th Nav  Changes

---
+++ src/components/scrollToTop/ScrollToTop.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import { FaArrowUp } from "react-icons/fa";
+import "./scrollToTop.css";
+
+const ScrollToTop = () => {
+  const [visible, setVisible] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 500) {
+        setVisible(true);
+      } else {
+        setVisible(false);
+      }
+    });
+  }, []);
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  return (
+    <div>
+      {visible && (
+        <button className="scroll-to-top" onClick={scrollTop}>
+          <FaArrowUp />
+        </button>
+      )}
+    </div>
+  );
+};
+
+export default ScrollToTop;
