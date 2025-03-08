96eb9ce723f4d80dd78aaaf2dad153aa37646287
Pradyumn Seth <pradyumnsethmzp123@gmail.com>
2025-03-09 00:34:36 +0530

    Header Image Changes Done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css';
 import CTA from './CTA'
-import ME from '../../assets/pd3_edited.png'
+import ME from '../../assets/image2.png'
 import HeaderSocials from './HeaderSocials';
 const Header = () => {
   return (
